1eb57cb8a3aa62da0b711321b14bf8b82251bd95
Rodrigo Attique <rodrigoatique@gmail.com>
2024-03-24 20:54:54 -0300

    testes

--- testes_automacao/executar.py
+++ testes_automacao/executar.py
@@ -1,19 +1,38 @@
 import pyautogui
 import time
 
-pyautogui.PAUSE = 1
-while False:
-    currentMouseX, currentMouseY = pyautogui.position() 
-    print(currentMouseX, currentMouseY)
-    time.sleep(4)
-pyautogui.position(0, 1)
-# pyautogui.hotkey("win", "r")
-# pyautogui.write("gpedit.msc")
-# pyautogui.write("notepad")
+pyautogui.PAUSE = 1.1
+# while True:
+#     currentMouseX, currentMouseY = pyautogui.position() 
+#     print(currentMouseX, currentMouseY)
+#     time.sleeppeRestroika23(4)
+for p in range(3):
+    pyautogui.hotkey("alt",'tab')
+# pyautogui.hotkey("win")
+# pyautogui.hotkey("cmd")
 # pyautogui.hotkey("enter")
-# time.sleep(5)
-# pyautogui.hotkey("Tab")
-# pyautogui.click(785, 242) # seleciona janela
-time.sleep(3)
-pyautogui.click(415, 461)
-# pyautogui.click()
+# time.sleep(10)
+# pyautogui.write("notepad c:\\windows\\system32\\drivers\\etc\\hosts")
+# pyautogui.write('runas /user:usuario "notepad c:\\windows\\system32\\drivers\\etc\\hosts"')
+# print('runas /user:usuario "notepad c:\\windows\\system32\\drivers\\etc\\hosts"')
+# pyautogui.hotkey("enter")
+# pyautogui.write("notepad.exe")
+# pyautogui.rightClick(x=224, y=274)
+# pyautogui.click(x=234, y=284)
+# pyautogui.hotkey("ctrl", "o") # abrir
+# for p in range(7): 
+#     pyautogui.hotkey("tab")
+# pyautogui.write("peRestroika23")
+# pyautogui.hotkey("enter")
+# pyautogui.write("c:\\windows\\system32\\drivers\\etc\\hosts")
+# pyautogui.hotkey("enter")
+pyautogui.hotkey("ctrl", "end")
+pyautogui.hotkey("enter")
+# pyautogui.write("127.0.0.1\tfriv.com")
+# fazer um for pra executar tudo isso acima
+
+
+
+
+time.sleep(4)
+
