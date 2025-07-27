c27d6f1561d6bcccc40330f2cfcd0b7e2b43aca8
Rodrigo Attique <rodrigoatique@gmail.com>
2025-07-27 16:37:22 -0300

    algoritmo muda valor

---
+++ iniciante/mudavalor.py
@@ -0,0 +1,19 @@
+#Algorítmo troca de valores
+
+a: int = 10
+b: int = 2
+print(f"a: {a} e b: {b}")
+# >>> a: 10 e b: 2
+
+a = b * a # altera valor de a
+print(f"a: {a} e b: {b}")
+# >>> a: 20 e b: 2
+
+b = b + a # altera o valor de b
+print(f"a: {a} e b: {b}")
+# >>> a: 20 e b: 22
+
+b = a - 4 # altera o valor de a
+print(f"a: {a} e b: {b}")
+# >>> a: 16 e b: 22
+
