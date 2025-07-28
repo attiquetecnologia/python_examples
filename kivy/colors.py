25f901a61e347840a6cf038d1de3331b2d4be28f
attiquetecnologia <rodrigoatique>
2025-07-28 09:46:40 -0300

    kivy testes

---
+++ kivy/colors.py
@@ -0,0 +1,7 @@
+class AppColors:
+    PASTEL_PINK = (1.0, 0.8, 0.8, 1.0)
+    PASTEL_BLUE = (0.7, 0.85, 1.0, 1.0)
+    PASTEL_GREEN = (0.8, 1.0, 0.8, 1.0)
+    PASTEL_YELLOW = (1.0, 1.0, 0.7, 1.0)
+    TEXT_DARK = (0.2, 0.2, 0.2, 1.0) # Para texto escuro
+    ACCENT_COLOR = (0.9, 0.5, 0.2, 1.0) # Cor de destaque
