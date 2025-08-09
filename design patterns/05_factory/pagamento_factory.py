ed5581de4dd0e570085ec9d713a956ea059e7ffb
Rodrigo Attique <rodrigoatique@gmail.com>
2025-08-09 09:32:14 -0300

    factory finalizado!

--- design patterns/05_factory/pagamento_factory.py
+++ design patterns/05_factory/pagamento_factory.py
@@ -1,12 +1,16 @@
 from pagamentos import PagamentoDinheiro, PagamentoPayPal
+from http_request import HttpRequest
 
 def pagamento_factory(tipo: str):
     match tipo:
         case 'dinheiro':
+            
             return PagamentoDinheiro()
         case 'online':
-            return PagamentoPayPal()
+            obj = HttpRequest()
+            return PagamentoPayPal(obj)
         case _:
             pass
 
-    
+pagamento = pagamento_factory("online")
+pagamento.pagar(555)
