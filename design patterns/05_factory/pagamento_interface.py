e3dd7dcb02b2013e67c9379da9f4d5e493a02e4f
Rodrigo Attique <rodrigoatique@gmail.com>
2025-08-09 09:54:17 -0300

    fontes

--- design patterns/05_factory/pagamento_interface.py
+++ design patterns/05_factory/pagamento_interface.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-
+# fontes: https://www.alura.com.br/artigos/design-patterns-python
+# https://www.youtube.com/watch?v=bPa6qDnZ-Ck
 class ServicoPagamentoInterface(ABC):
     @abstractmethod
     def pagar(self, valor: float):
         # metodo obrigatório no filho
         pass
 
-        
