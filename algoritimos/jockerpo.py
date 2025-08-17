26ff4177925b46ac2693dfa9ecd4c26488237f73
Rodrigo Attique <rodrigoatique@gmail.com>
2025-08-17 17:03:07 -0300

    Clean

--- algoritimos/jockerpo.py
+++ algoritimos/jockerpo.py
@@ -1,22 +1,25 @@
-# Jocker Po - Pedra, Papel,Tesoura
-# Jogo para dois jogadores sem interação com usuário
-# Pedra > Tesoura > Papel > Pedra
-# Se Jogador1: Papel e Jogador2: Pedra, Jogador1
-# Uso da função random do python
+""" Jocker Po - Pedra, Papel,Tesoura
+Jogo para dois jogadores sem interação com usuário
+Pedra > Tesoura > Papel > Pedra
+Se Jogador1: Papel e Jogador2: Pedra, Jogador1
+Uso da função random do python
 
-# O grande problema do algorítimo é arranjar uma forma inteligente de jogar e permitir que 
-# outros jogadores entrem no jogo da forma escolhida isso não é possível.
+O grande problema do algorítimo é arranjar uma forma inteligente de jogar e permitir que 
+outros jogadores entrem no jogo da forma escolhida isso não é possível.
+"""
 
 # Começar pela jogada, prototipo
 import random
 from enum import Enum
 
 class Escolha(Enum):
+    """ Enum para escolhas"""
     PEDRA = 0
     PAPEL = 1
     TESOURA = 2
 
 class Jogador:
+    """ Classe de Jogador """
     def __init__(self, nome: str):
         self.nome: str = nome
         # Escolhe na instancia
@@ -26,13 +29,16 @@ class Jogador:
         return f"Jogador {self.nome}, escolheu: {self.escolha}"
 
 class Jogada:
+    """ Classe de Jogada """
     def __init__(self, jogador_a: Jogador, jogador_b: Jogador):
         self.jogador_a: Jogador = jogador_a
         self.jogador_b: Jogador = jogador_b
         self.comparar()
 
     def comparar(self) -> str:
+        """ Comparar Jogadas """
         print(self.jogador_a, self.jogador_b)
+        
         if self.jogador_a.escolha == Escolha.PEDRA and self.jogador_b.escolha  == Escolha.TESOURA:
             print("Jogador A venceu")
         elif self.jogador_a.escolha == Escolha.TESOURA and self.jogador_b.escolha  == Escolha.PAPEL:
