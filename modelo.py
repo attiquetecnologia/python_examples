53790dfdc3c5e33ad83ad7fe3a3564e918864fef
Rodrigo Attique <rodrigoatique@gmail.com>
2026-09-01 21:14:31 -0300

    modelo de cabeçalho

---
+++ modelo.py
@@ -0,0 +1,7 @@
+"""Descricao: Breve descrição do que o arquivo faz, exemplo, arquivo de funções de banco de dados, ou, leitura e escrita de arquivos…. 
+nome_arquivo: processador_notas.py 
+nome_exercicio: Atividade 01 - Práticas de Clean Code Autor: 
+nome_aluno: Seu nome completo
+email_aluno: seu.email@exemplo.com 
+turma: código da turma
+"""
